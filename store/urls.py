c18aec5fd39405c64f40069410e847674e184ce6
omi <59523166+omi265@users.noreply.github.com>
2020-11-12 01:21:55 +0530

    Adding base navbar

--- store/urls.py
+++ store/urls.py
@@ -1,10 +1,9 @@
-"""
 from django.urls import path, include
+from . import views
 
 urlpatterns = [
-    path('', views.Index.as_view(), name='index'),
-    path('signup', views.Signup.as_view(), name='signup'),
-    path('login', views.Login.as_view(), name='login'),
-    path('logout', views.logout, name='logout'),
+    path('', views.index, name='index'),
+    # path('signup', views.Signup.as_view(), name='signup'),
+    # path('login', views.Login.as_view(), name='login'),
+    # path('logout', views.logout, name='logout'),
 ]
-"""
--- store/views.py
+++ store/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+
+
+def index(request):
+    return render(request, 'store/home.html')
 
 # Create your views here.
