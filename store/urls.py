e045fe82640fcf45bcc178cb27a50460973f05eb
omi <59523166+omi265@users.noreply.github.com>
2020-11-19 22:08:00 +0530

    Adding Pages

--- store/urls.py
+++ store/urls.py
@@ -3,6 +3,8 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('flights', views.all_flights, name='flights'),
+    path('hotels', views.all_hotels, name='hotels'),
     # path('signup', views.Signup.as_view(), name='signup'),
     # path('login', views.Login.as_view(), name='login'),
     # path('logout', views.logout, name='logout'),
--- store/views.py
+++ store/views.py
@@ -5,4 +5,10 @@ from django.http import HttpResponse
 def index(request):
     return render(request, 'store/home.html')
 
+def all_flights(request):
+    return render(request, 'store/flights.html')
+
+def all_hotels(request):
+    return render(request, 'store/hotels.html')
+
 # Create your views here.
